16d5201b9c31b6a82445263905d53afcf8579f5c
carlosa3010 <carlosa.cm835@gmail.com>
2026-01-18 05:30:24 +0000

    Auto-update: Cambio detectado en show.blade.php

--- routes/web.php
+++ routes/web.php
@@ -50,9 +50,10 @@ Route::prefix('admin')->name('admin.')->group(function () {
     Route::get('accounts/create', [AccountController::class, 'create'])->name('accounts.create');
     Route::post('accounts', [AccountController::class, 'store'])->name('accounts.store');
     
-    // Ficha Técnica y Edición General
+    // Ficha Técnica, Edición y Eliminación
     Route::get('accounts/{id}', [AccountController::class, 'show'])->name('accounts.show');
     Route::put('accounts/{id}', [AccountController::class, 'update'])->name('accounts.update');
+    Route::delete('accounts/{id}', [AccountController::class, 'destroy'])->name('accounts.destroy'); // <--- ESTA FALTABA
 
     // Actualización de Notas Operativas (Permanentes/Temporales)
     Route::put('accounts/{id}/notes', [AccountController::class, 'updateNotes'])->name('accounts.notes.update');
