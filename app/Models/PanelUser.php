0b346b6643ec984ce063c748ab2fe092c34c8d8c
carlosa3010 <carlosa.cm835@gmail.com>
2026-01-18 06:00:54 +0000

    Auto-update: Cambio detectado en PanelUser.php

--- app/Models/PanelUser.php
+++ app/Models/PanelUser.php
@@ -2,9 +2,22 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class PanelUser extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'alarm_account_id',
+        'user_number', // Slot en el panel (001, 002...)
+        'name',        // Nombre de la persona (Ej: Gerente Pedro)
+        'role'         // master, user, duress, etc.
+    ];
+
+    public function account()
+    {
+        return $this->belongsTo(AlarmAccount::class, 'alarm_account_id');
+    }
 }
