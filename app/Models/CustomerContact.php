2f253522b9d2392609c7145309460ee5f8dc521f
carlosa3010 <carlosa.cm835@gmail.com>
2026-01-19 00:56:26 +0000

    Auto-update: Cambio detectado en CustomerContact.php

--- app/Models/CustomerContact.php
+++ app/Models/CustomerContact.php
@@ -2,9 +2,23 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class CustomerContact extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'customer_id',
+        'name',
+        'relationship',
+        'phone',
+        'is_active'
+    ];
+
+    public function customer()
+    {
+        return $this->belongsTo(Customer::class);
+    }
 }
