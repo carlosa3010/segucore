3aa1410fdf2d9269de06c475e6fd7a964975b89a
carlosa3010 <carlosa.cm835@gmail.com>
2026-01-19 01:30:42 +0000

    Auto-update: Cambio detectado en SiaCode.php

--- app/Models/SiaCode.php
+++ app/Models/SiaCode.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class SiaCode extends Model
 {
-    //
+    protected $fillable = [
+        'code',         // Ej: BA, RP, OP
+        'name',         // Ej: Alarma de Robo, Test Automático
+        'description',  // Instrucciones para el operador
+        'priority',     // 0=Log (Ignorar), 1=Info (Monitor), 2=Alarma (Sonar), 3=Pánico
+        'category'      // 'alarm', 'status', 'test', 'access'
+    ];
 }
