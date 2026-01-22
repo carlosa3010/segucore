5ef8b091707b4914acdbc64781dcc175c277ce28
carlosa3010 <carlosa.cm835@gmail.com>
2026-01-22 05:51:45 +0000

    Auto-update: Cambio detectado en TraccarDevice.php

--- app/Models/TraccarDevice.php
+++ app/Models/TraccarDevice.php
@@ -1,4 +1,4 @@
-<?php
+<<?php
 
 namespace App\Models;
 
@@ -6,12 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class TraccarDevice extends Model
 {
-    // ESTA ES LA MAGIA: Usar la conexión de lectura de Traccar
     protected $connection = 'traccar'; 
-
-    // La tabla real en la BD de Traccar
     protected $table = 'tc_devices';
-
-    // Traccar no usa los timestamps de Laravel (created_at, updated_at) por defecto
     public $timestamps = false;
+
+    // --- AGREGAR ESTA RELACIÓN ---
+    // Un dispositivo tiene una "última posición" referenciada por positionid
+    public function position()
+    {
+        return $this->hasOne(TraccarPosition::class, 'id', 'positionid');
+    }
 }
