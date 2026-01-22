668b740c10f22f6342d500547c2228dfde971f38
carlosa3010 <carlosa.cm835@gmail.com>
2026-01-22 03:40:00 +0000

    Auto-update: Cambio detectado en ServicePlan.php

--- app/Models/ServicePlan.php
+++ app/Models/ServicePlan.php
@@ -6,5 +6,28 @@ use Illuminate\Database\Eloquent\Model;
 
 class ServicePlan extends Model
 {
-    //
+    protected $fillable = [
+        'name', 
+        'price', 
+        'currency', 
+        'billing_cycle_days',
+        'description' // Sugiero agregar este campo en una futura migración si no existe
+    ];
+
+    protected $casts = [
+        'price' => 'decimal:2',
+        'billing_cycle_days' => 'integer',
+    ];
+
+    // Relación: Un plan tiene muchas cuentas suscritas
+    public function accounts()
+    {
+        return $this->hasMany(AlarmAccount::class);
+    }
+    
+    // Helper para mostrar precio formateado
+    public function getFormattedPriceAttribute()
+    {
+        return $this->currency . ' ' . number_format($this->price, 2);
+    }
 }
