404a35359377200f75e291b30ae1f458d9a90401
carlosa3010 <carlosa.cm835@gmail.com>
2026-01-22 07:04:28 +0000

    Auto-update: Cambio detectado en Import.php

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,20 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote');
+})->purpose('Display an inspiring quote')->hourly();
+
+/**
+ * --------------------------------------------------------------------------
+ * TAREAS PROGRAMADAS (CRON JOBS)
+ * --------------------------------------------------------------------------
+ */
+
+// Verificar alertas de GPS (Exceso de velocidad, etc.) cada minuto
+Schedule::command('gps:check-alerts')->everyMinute();
+
+// Limpiar tokens de API expirados diariamente (Mantenimiento)
+Schedule::command('sanctum:prune-expired')->daily();
