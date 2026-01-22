9f028e57ee72d77468a59650add7354af7cbc63d
carlosa3010 <carlosa.cm835@gmail.com>
2026-01-22 05:32:11 +0000

    Auto-update: Cambio detectado en GpsDevice.php

--- app/Models/GpsDevice.php
+++ app/Models/GpsDevice.php
@@ -3,8 +3,35 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class GpsDevice extends Model
 {
-    //
+    use SoftDeletes;
+
+    protected $fillable = [
+        'customer_id',
+        'name',
+        'imei', // <--- CAMBIO AQUI
+        'phone_number',
+        'sim_card_id',
+        'device_model',
+        'plate_number',
+        'vehicle_type',
+        'driver_name',
+        'installation_date',
+        'subscription_status'
+    ];
+
+    public function customer()
+    {
+        return $this->belongsTo(Customer::class);
+    }
+
+    // Relación con Traccar
+    public function traccarData()
+    {
+        // Enlace: 'imei' local <---> 'uniqueid' remoto
+        return $this->setConnection('traccar')->hasOne(TraccarDevice::class, 'uniqueid', 'imei');
+    }
 }
