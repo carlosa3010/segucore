608589c4a61beddb50a02a54ff862c2f4a186c7e
carlosa3010 <carlosa.cm835@gmail.com>
2026-01-22 07:10:32 +0000

    Auto-update: Cambio detectado en iso-8859-11.map

--- app/Models/Driver.php
+++ app/Models/Driver.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Driver extends Model
 {
-    //
+    protected $fillable = ['full_name', 'license_number', 'phone', 'email', 'photo_path', 'status'];
+
+    public function devices()
+    {
+        return $this->hasMany(GpsDevice::class);
+    }
 }
