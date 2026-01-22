36bd0833ee731d85ed8844c00a17f84b65581818
carlosa3010 <carlosa.cm835@gmail.com>
2026-01-22 07:32:19 +0000

    Auto-update: Cambio detectado en solr.php

--- app/Models/Geofence.php
+++ app/Models/Geofence.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Geofence extends Model
 {
-    //
+    protected $fillable = [
+        'name', 
+        'description', 
+        'area', 
+        'traccar_geofence_id'
+    ];
+
+    /**
+     * Relación: Una geocerca puede estar asignada a muchos dispositivos.
+     */
+    public function devices()
+    {
+        return $this->belongsToMany(GpsDevice::class, 'geofence_gps_device');
+    }
 }
