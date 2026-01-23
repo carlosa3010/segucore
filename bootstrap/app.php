4f6ef480edf8cd1c2061766dbb578557a5bc4c3b
carlosa3010 <carlosa.cm835@gmail.com>
2026-01-23 21:02:42 +0000

    Auto-update: Cambio detectado en app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -7,12 +7,16 @@ use Illuminate\Foundation\Configuration\Middleware;
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php', // <--- ASEGÚRATE DE QUE ESTA LÍNEA EXISTA
+        api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        // --- AGREGAR ESTO ---
+        $middleware->alias([
+            'video.wall' => \App\Http\Middleware\EnsureVideoWallAccess::class,
+            // 'role' => \Spatie\Permission\Middleware\Role::class, // (Recomendado instalar a futuro)
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
