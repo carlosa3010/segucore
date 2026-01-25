c732895881f9aee2130c4f061d810b80b0e8cae9
carlosa3010 <carlosa.cm835@gmail.com>
2026-01-25 00:49:33 +0000

    Auto-update: Cambio detectado en Invoice.php

--- app/Models/Invoice.php
+++ app/Models/Invoice.php
@@ -6,5 +6,30 @@ use Illuminate\Database\Eloquent\Model;
 
 class Invoice extends Model
 {
-    //
+    protected $fillable = [
+        'customer_id',
+        'invoice_number',
+        'issue_date',
+        'due_date',
+        'subtotal', // Agregamos subtotal e iva si deseas, por ahora total directo
+        'total',
+        'status', // unpaid, paid, cancelled
+        'details' // JSON para guardar el desglose (ej: 5 GPS x $10)
+    ];
+
+    protected $casts = [
+        'issue_date' => 'date',
+        'due_date' => 'date',
+        'details' => 'array'
+    ];
+
+    public function customer()
+    {
+        return $this->belongsTo(Customer::class);
+    }
+
+    public function payments()
+    {
+        return $this->hasMany(Payment::class);
+    }
 }
